b6244093ea8275af975616c0f57b9625906d3234
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2020-09-30 11:44:25 +0200

    Created the project

---
+++ Contact list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact list
+//
+//  Created by Reinaldo Villanueva Javierre on 30/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
