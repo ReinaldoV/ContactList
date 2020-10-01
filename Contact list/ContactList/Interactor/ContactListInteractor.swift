2a071a854e9260a44757a3a03701035c37f8396f
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2020-10-01 21:52:45 +0200

    Added a sort function by name of the contacts

--- Contact list/ContactList/Interactor/ContactListInteractor.swift
+++ Contact list/ContactList/Interactor/ContactListInteractor.swift
@@ -22,6 +22,27 @@ class ContactListInteractor {
 extension ContactListInteractor: ContactListInteractorProtocol {
 
     func loadContacts(onSuccess: @escaping ([Contact]) -> Void, onError: @escaping (Error?) -> Void) {
-        contactsService.getContacts(onSuccess: onSuccess, onError: onError)
+        contactsService.getContacts(onSuccess: {contacts in
+            let sortedContacts = contacts.sorted(by: { (lhs, rhs) -> Bool in
+                
+                if let lhsName = lhs.name, let rhsName = rhs.name {
+                    return lhsName < rhsName
+                }
+
+                if lhs.name == nil && rhs.name == nil {
+                    return false
+                }
+
+                if lhs.name == nil {
+                    return false
+                }
+
+                if rhs.name == nil {
+                    return true
+                }
+                return true
+            })
+            onSuccess(sortedContacts)
+        }, onError: onError)
     }
 }
